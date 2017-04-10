01527e3e232aae772ecdd917994c6480ed4d9819
Andrey Petrov <andrey.petrov@gmail.com>
2017-04-10 13:56:39 -0700

    Add PyPi
    Signed-off-by: Andrey Petrov <andrey.petrov@gmail.com>

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import pkgconfig
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 cflags = pkgconfig.cflags('purple')
 ldflags = pkgconfig.libs('purple')
@@ -18,19 +19,21 @@ class pypurple_build_ext(build_ext):
         self.include_dirs.insert(0, 'libpurple')
         self.pyrex_include_dirs.extend(self.include_dirs)
 
+sourcefiles = ['purple/purple.pyx', 'purple/c_purple.c']
+extensions = [Extension("purple", sourcefiles,
+                        extra_compile_args=cflags.split(),
+                        extra_link_args=ldflags.split())]
 setup(
     name='purple',
     version='0.0.1',
     author='Andrey Petrov',
     author_email='andrey.petrov@gmail.com',
+    packages=['purple'],
     description='Python bindings for Purple',
+    url="https://github.com/anpetrov/python-purple",
+    download_url="https://github.com/anpetrov/python-purple/archive/0.0.1.tar.gz",
     long_description=long_description,
-
-    ext_modules=[
-        Extension('c_purple.c', ['purple.pyx'],
-                  extra_compile_args=cflags.split(),
-                  extra_link_args=ldflags.split())
-    ],
+    include_package_data=True,
+    ext_modules=cythonize(extensions, include_path=["purple/libpurple"]),
     cmdclass={'build_ext': pypurple_build_ext}
-
 )
